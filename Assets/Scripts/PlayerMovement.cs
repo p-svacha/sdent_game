eb8512587762dffc80638a5e5c10d9d85d0c74ae
Philip Svacha <philip.svacha@gmail.com>
2016-04-06 15:36:30 +0200

    added walking and idle

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody2D rBody;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		rBody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //on or off, not linear
+
+        if(movementVector != Vector2.zero)
+        {
+            anim.SetBool("is_walking", true);
+            anim.SetFloat("input_x", movementVector.x);
+            anim.SetFloat("input_y", movementVector.y);
+        }
+        else
+        {
+            anim.SetBool("is_walking", false);
+        }
+
+        rBody.MovePosition(rBody.position + movementVector * Time.deltaTime);
+	}
+}
